dadabcb9e936b2bb4ec07c2e6bc62bd87e849284
Victor Gueorguiev <victor@amplifyanalytix.com>
2021-06-09 08:22:11 +0300

    Adjusted parameters in python script.

--- src/Exercise1/Exercise1.py
+++ src/Exercise1/Exercise1.py
@@ -8,9 +8,6 @@ import matplotlib.pyplot as plt
 import os 
 import findspark
 
-spark_location = "/cw/bdap/software/spark-2.4.0-bin-hadoop2.7"
-os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64/'
-data_folder = '/cw/bdap/assignment3/'
 #spark_location = "C:\\Users\\VictorGueorguiev\\Documents\\Spark\\spark-2.4.5-bin-hadoop2.7"
 #os.environ['JAVA_HOME'] = "C:\\Program Files\\Java\\jdk1.8.0_161"
 findspark.init(spark_location)
@@ -21,7 +18,7 @@ import pyspark.sql.functions as F
 import math
 from datetime import datetime
 import time
-
+import argparse
 ## returns result in kilometres
 ## this is more accurate that using the flat earth version defined below
 def haversine(lat1, lon1, lat2, lon2):
@@ -57,6 +54,28 @@ def spherical_projection(lat1, lon1, lat2, lon2):
     distance = R * math.sqrt(dlat**2 + (math.cos(midlat) * dlon)**2) * 1000
     return distance
 
+
+# Model Settings =================================================================================================
+parser = argparse.ArgumentParser(description='Exercise1',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--dataset_name', type=str, default='./data/2010_03.trips',
+                    help='Dataset name and full path.')
+parser.add_argument('--spark_location', type=str, default="/cw/bdap/software/spark-2.4.0-bin-hadoop2.7", help='Spark install location, default on cw/bdap...')
+parser.add_argument('--java_home', type=str, default='/usr/lib/jvm/java-8-openjdk-amd64/', help='JAVA HOME env variable, default is /usr/lib/jvm/java-8-openjdk-amd64/')
+parser.add_argument('--do_plotting', type=bool, default=False, help='Should we plot results? Default is False since running on remote machines.')
+
+opt = parser.parse_args()
+
+#spark_location = "/cw/bdap/software/spark-2.4.0-bin-hadoop2.7"
+#os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64/'
+#data_folder = '/cw/bdap/assignment3/'
+
+spark_location = opt.spark_location
+os.environ['JAVA_HOME'] = opt.java_home
+dataset_name = opt.dataset_name
+do_plotting = opt.do_plotting
+
 ### TESTING SPARK CONNECTION
 #spark = SparkSession.builder.getOrCreate()
 #df = spark.sql("select 'spark' as hello ")
@@ -76,7 +95,7 @@ sqlContext = SQLContext(sc)
 ## Homemade calculations, including loading data
 
 
-f = open(data_folder + "2010_03.trips", "r")
+f = open(dataset_name, "r")
 
 start = time.time()
 
@@ -103,8 +122,9 @@ f.close()
 
 # plot data in RDD - use .collect() to bring data to local
 num_bins = 35
-n, bins, patches = plt.hist(homemade_trip_distribution, num_bins, normed=0, facecolor='green', alpha=0.5, range=[0, 35])
-n, bins, patches = plt.hist(homemade_trip_distribution, num_bins, normed=1, facecolor='green', alpha=0.5, range=[0, 35])
+if do_plotting:
+    n, bins, patches = plt.hist(homemade_trip_distribution, num_bins, normed=0, facecolor='green', alpha=0.5, range=[0, 35])
+    n, bins, patches = plt.hist(homemade_trip_distribution, num_bins, normed=1, facecolor='green', alpha=0.5, range=[0, 35])
 
 
 ### PART B - TRIP DISTANCE DISTRIBUTION USING PYSPARK
@@ -112,7 +132,7 @@ n, bins, patches = plt.hist(homemade_trip_distribution, num_bins, normed=1, face
 ## PySpark RDD API calculations, including loading data
 
 
-text_file = sc.textFile(data_folder + "2010_03.trips")
+text_file = sc.textFile(dataset_name)
 
 start = time.time()
 
@@ -141,8 +161,9 @@ print("Time elapsed for PySpark RDD API trip distance distribution: ", str(elaps
 
 # plot data in RDD - use .collect() to bring data to local
 num_bins = 36
-n, bins, patches = plt.hist(pyspark_trip_distribution, num_bins, normed=0, facecolor='green', alpha=0.5,  range=[0, 35])
-n, bins, patches = plt.hist(pyspark_trip_distribution, num_bins, normed=1, facecolor='green', alpha=0.5,  range=[0, 35])
+if do_plotting:
+    n, bins, patches = plt.hist(pyspark_trip_distribution, num_bins, normed=0, facecolor='green', alpha=0.5,  range=[0, 35])
+    n, bins, patches = plt.hist(pyspark_trip_distribution, num_bins, normed=1, facecolor='green', alpha=0.5,  range=[0, 35])
 
 
         
